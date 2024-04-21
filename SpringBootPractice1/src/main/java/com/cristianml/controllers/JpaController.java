d653ea438635d0676ff5662f6d79f7b935e04d61
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-04-21 19:26:07 -0400

    Listando las categorias con JPA

--- SpringBootPractice1/src/main/java/com/cristianml/controllers/JpaController.java
+++ SpringBootPractice1/src/main/java/com/cristianml/controllers/JpaController.java
@@ -1,15 +1,28 @@
 package com.cristianml.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cristianml.services.CategoriaService;
 
 @Controller
 @RequestMapping("")
 public class JpaController {
+	
+	@Autowired
+	private CategoriaService categoriaService;
 
 	@GetMapping("/jpa-repository")
 	public String home() {
 		return "jpa_repository/home";
+	}
+	
+	@GetMapping("/jpa-repository/categorias")
+	public String categorias(Model model) {
+		model.addAttribute("datos", this.categoriaService.listar());
+		return "jpa_repository/categorias";
 	}
 }
--- SpringBootPractice1/src/main/java/com/cristianml/services/CategoriaService.java
+++ SpringBootPractice1/src/main/java/com/cristianml/services/CategoriaService.java
@@ -1,9 +1,12 @@
 package com.cristianml.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+import com.cristianml.models.CategoriaModel;
 import com.cristianml.repositories.ICategoriaRepository;
 
 @Service
@@ -14,4 +17,9 @@ public class CategoriaService {
 	
 	@Autowired
 	private ICategoriaRepository repository;
+	
+	// Listar
+	public List<CategoriaModel> listar() {
+		return repository.findAll();
+	}
 }
